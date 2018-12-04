d84e9fe95230c626656490f75daf967b2a9c7fff
Pusheeneiro <jankowalski413d@gmail.com>
2018-12-05 00:00:14 +0100

    Movement & MapWalls Complete

--- Klapacz/Assets/Scripts/Pac.cs
+++ Klapacz/Assets/Scripts/Pac.cs
@@ -6,40 +6,64 @@ public class Pac : MonoBehaviour {
 
     private Rigidbody2D PacBody;
     private Vector2 PacVer;
+    private GameObject ColideBox;
 
     private bool isColideDOWN;
-    public Transform groundCheck;
-    public Vector2 checkSize;
-    public LayerMask whatIsGround;
+    private bool isColideUP;
+    private bool isColideLEFT;
+    private bool isColideRIGHT;
+
+    private Transform DownColisionCheck;
+    private Transform UpColisionCheck;
+    private Transform LeftColisionCheck;
+    private Transform RightColisionCheck;
+
+    private Vector2 BoxSize;
+    private LayerMask Walls;
 
     void Start()
     {
         PacBody = GetComponent<Rigidbody2D>();
         PacVer = new Vector2(1,0);
+
         isColideDOWN = false;
+        isColideUP = false;
+        isColideLEFT = false;
+        isColideRIGHT = false;
+
+        BoxSize = new Vector2(0.03f, 0.03f);
+        Walls = LayerMask.GetMask("walls");
+        ColideBox = GameObject.Find("down");
+        DownColisionCheck = ColideBox.transform;
+        ColideBox = GameObject.Find("up");
+        UpColisionCheck = ColideBox.transform;
+        ColideBox = GameObject.Find("left");
+        LeftColisionCheck = ColideBox.transform;
+        ColideBox = GameObject.Find("right");
+        RightColisionCheck = ColideBox.transform;
     }
     
     void Update ()
     {
         
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.A) && !isColideLEFT)
         {
-            PacVer.x = -1;
+            PacVer.x = -0.65f;
             PacVer.y = 0;
         }
-        if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKeyDown(KeyCode.D) && !isColideRIGHT)
         {
-            PacVer.x = 1;
+            PacVer.x = 0.65f;
             PacVer.y = 0;
         }
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.W) && !isColideUP)
         {
-            PacVer.y = 1;
+            PacVer.y = 0.65f;
             PacVer.x = 0;
         }
         if (Input.GetKeyDown(KeyCode.S) && !isColideDOWN)
         {
-            PacVer.y = -1;
+            PacVer.y = -0.65f;
             PacVer.x = 0;
         }
         
@@ -48,18 +72,24 @@ public class Pac : MonoBehaviour {
     }
     void FixedUpdate()
     {
+        //Move
         PacBody.MovePosition(PacBody.position + PacVer * Time.fixedDeltaTime);
 
-        if (Physics2D.OverlapBox(groundCheck.position, checkSize, 0, whatIsGround))
-        {
-            isColideDOWN = true;
-            Debug.Log("true");
-        }
-        else {
-            isColideDOWN = false;
-            Debug.Log("false");
-        }
+        //S key block
+        if (Physics2D.OverlapBox(DownColisionCheck.position, BoxSize, 0, Walls))isColideDOWN = true;
+        else isColideDOWN = false;
 
+        //W key block
+        if (Physics2D.OverlapBox(UpColisionCheck.position, BoxSize, 0, Walls)) isColideUP = true;
+        else isColideUP = false;
+
+        //A key block
+        if (Physics2D.OverlapBox(LeftColisionCheck.position, BoxSize, 0, Walls)) isColideLEFT = true;
+        else isColideLEFT = false;
+
+        //D key block
+        if (Physics2D.OverlapBox(RightColisionCheck.position, BoxSize, 0, Walls)) isColideRIGHT = true;
+        else isColideRIGHT = false;
     }
 
     void OnTriggerStay2D(Collider2D other)
