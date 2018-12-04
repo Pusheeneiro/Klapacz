c5704c46e2a6f60016729384a1d281f1e52b5210
C4mi110 <45486615+C4mi110@users.noreply.github.com>
2018-12-04 22:32:01 +0100

    movement

--- Klapacz/Assets/Scripts/Pac.cs
+++ Klapacz/Assets/Scripts/Pac.cs
@@ -7,14 +7,62 @@ public class Pac : MonoBehaviour {
     private Rigidbody2D PacBody;
     private Vector2 PacVer;
 
+    private bool isColideDOWN;
+    public Transform groundCheck;
+    public Vector2 checkSize;
+    public LayerMask whatIsGround;
 
     void Start()
     {
         PacBody = GetComponent<Rigidbody2D>();
         PacVer = new Vector2(1,0);
+        isColideDOWN = false;
     }
     
     void Update ()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            PacVer.x = -1;
+            PacVer.y = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            PacVer.x = 1;
+            PacVer.y = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            PacVer.y = 1;
+            PacVer.x = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.S) && !isColideDOWN)
+        {
+            PacVer.y = -1;
+            PacVer.x = 0;
+        }
+        
+
+
+    }
+    void FixedUpdate()
+    {
+        PacBody.MovePosition(PacBody.position + PacVer * Time.fixedDeltaTime);
+
+        if (Physics2D.OverlapBox(groundCheck.position, checkSize, 0, whatIsGround))
+        {
+            isColideDOWN = true;
+            Debug.Log("true");
+        }
+        else {
+            isColideDOWN = false;
+            Debug.Log("false");
+        }
+
+    }
+
+    void OnTriggerStay2D(Collider2D other)
     {
         /*
         if (Input.GetKeyDown(KeyCode.A))
@@ -38,36 +86,6 @@ public class Pac : MonoBehaviour {
             PacVer.x = 0;
         }
         */
-
-
-    }
-    void FixedUpdate()
-    {
-        PacBody.MovePosition(PacBody.position + PacVer * Time.fixedDeltaTime);
-    }
-
-    void OnTriggerStay2D(Collider2D other)
-    {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            PacVer.x = -1;
-            PacVer.y = 0;
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            PacVer.x = 1;
-            PacVer.y = 0;
-        }
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            PacVer.y = 1;
-            PacVer.x = 0;
-        }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            PacVer.y = -1;
-            PacVer.x = 0;
-        }
     }
 
 
